e56ddb84536065786e0a55143faa8c6fc035c119
Aga <a.dufrat@gmail.com>
2019-02-15 12:08:37 +0000

    Make this plugin compatible with python docker library
    The upstream uses the older docker-py library but for gov.uk we are using
    docker-compose, as the docker library is shared by both this commit adjusts the
    docker-collectd-plugin to work with the python docker library.

--- dockerplugin.py
+++ dockerplugin.py
@@ -159,7 +159,7 @@ class MemoryStats(Stats):
         for key, value in (mem_stats.get('stats') or {}).items():
             cls.emit(container, 'memory.stats', [value],
                      type_instance=key, t=t)
-        
+
         mem_usage_no_cache = mem_stats['usage'] - mem_stats['stats']['cache']
         mem_percent = 100.0 * mem_usage_no_cache / mem_stats['limit']
         cls.emit(container, 'memory.percent', ["%.2f" % mem_percent], t=t)
@@ -280,7 +280,7 @@ class DockerPlugin:
                 self.timeout = int(node.values[0])
 
     def init_callback(self):
-        self.client = docker.Client(
+        self.client = docker.APIClient(
             base_url=self.docker_url,
             version=DockerPlugin.MIN_DOCKER_API_VERSION)
         self.client.timeout = self.timeout
